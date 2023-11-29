22324a863363c4ea56dafe5bcc29e2147b3fe5c4
hansie4 <hans.vongruenigen@gmail.com>
2023-11-29 14:27:33 -0500

    Day 9 done

--- 2015/Day 09/main.py
+++ 2015/Day 09/main.py
@@ -17,50 +17,99 @@ def readInRoutes():
     return a
 
 
-def shortestPath(paths: dict, node: str, visited: list, currentCost: int):
-    print("Looking for path from ", node)
+# Needs to take a list of paths a current route and output a list of the routes that can be taken from the current route
+def shortestPath(nodes: set, paths: dict, route: list):
+    # print("Looking for path from ", route)
     # Base case all have been met or current node has none it can travel to
-    lastNode = len(paths.keys()) > (len(visited) - 1)
-    cantTravel = paths[node] == None
-    if not cantTravel:
-        for x in paths[node]:
-            if x not in visited:
-                cantTravel = False
 
-    if lastNode or cantTravel:
-        return currentCost
+    if len(route) == len(nodes):
+        # print("Route Found: ", route)
 
-    return cost
+        d = 0
+
+        for x in range(len(route) - 1):
+            f = route[x]
+            t = route[x + 1]
+
+            d += paths[f][t]
+
+        return route, d
+
+    cityFrom = route[:1].pop()
+
+    if len(paths[cityFrom].keys()) == 0:
+        return None
+
+    routeList = list()
+
+    for x in paths[cityFrom].keys():
+        if x not in route:
+            newRoute = route.copy()
+            newRoute.append(x)
+            # print("New route: ", newRoute)
+            newPath = shortestPath(nodes, paths, newRoute)
+
+            if newPath:
+                routeList.append(newPath)
+
+    return routeList
 
 
 def pt1():
     r = readInRoutes()
 
-    # print(r)
-
     paths = dict()
 
-    for x in r:
-        if x[0] in paths:
-            paths[x[0]][x[1]] = x[2]
-        else:
-            paths[x[0]] = {}
-            paths[x[0]][x[1]] = int(x[2])
+    nodes = set()
 
     for x in r:
-        if x[1] not in paths:
-            paths[x[1]] = None
+        nodes.add(x[0])
+        nodes.add(x[1])
 
-    print(paths)
+    for x in nodes:
+        paths[x] = {}
 
-    print(shortestPath(paths, [], "London"))
+    for x in r:
+        paths[x[0]][x[1]] = int(x[2])
+        paths[x[1]][x[0]] = int(x[2])
 
+    # print(nodes)
+    # print(paths)
 
-def pt2():
-    pass
+    routes = list()
+
+    for x in nodes:
+        p = shortestPath(nodes, paths, [x])
+        for z in p:
+            routes.append(z)
+
+    # routes.sort(key=lambda x: x[0][1])
+
+    flattened = list()
+    # print("---------------------")
+    for x in routes:
+        for y in x:
+            for z in y:
+                for aa in z:
+                    for bb in aa:
+                        for zz in bb:
+                            flattened.append(zz[0])
+
+    flattened.sort(key=lambda z: z[1])
+    flattened.reverse()
+
+    # for x in flattened:
+    #     print(x)
+
+    ans1 = flattened.pop()[1]
+    flattened.reverse()
+    ans2 = flattened.pop()[1]
+
+    return ans1, ans2
 
 
 print("Part 1 Answer:")
-pt1()
+ans = pt1()
+print(ans[0])
 print("Part 2 Answer:")
-pt2()
+print(ans[1])
