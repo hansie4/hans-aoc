bb4bc5d1b63d3b448aad09264385c3f709c0a725
hansie4 <hans.vongruenigen@gmail.com>
2023-11-29 18:22:42 -0500

    Day 10

--- 2015/Day 10/main.py
+++ 2015/Day 10/main.py
@@ -9,8 +9,48 @@ f = open(abs_file_path, "r")
 data = f.read()
 
 
+def lookAndSay(val: str):
+    cutUp = [""] * len(val)
+
+    i = -1
+    lastVal = ""
+    for x in val:
+        if x != lastVal:
+            i += 1
+
+        lastVal = x
+
+        cutUp[i] = cutUp[i] + lastVal
+
+    # print(cutUp)
+    for x in range(len(cutUp)):
+        if cutUp[x]:
+            cutUp[x] = newString(cutUp[x])
+    # print(cutUp)
+
+    new = "".join(cutUp)
+    # print(new)
+    return new
+
+
+def newString(val: str):
+    ch = val[0]
+    ln = len(val)
+    return str(ln) + ch
+
+
 def pt1():
-    pass
+    print(data)
+    temp = data
+    for x in range(50):
+        print("Run: ", x + 1)
+        temp = lookAndSay(temp)
+        # print(temp)
+    print(len(temp))
+
+    # f = open(os.path.join(script_dir, "output.txt"), "w")
+    # f.write(temp)
+    # f.close()
 
 
 def pt2():
