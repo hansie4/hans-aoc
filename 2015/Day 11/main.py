22c9a386c344192573a0f1b08852a9d248ebdcd1
hansie4 <hans.vongruenigen@gmail.com>
2023-11-29 21:30:54 -0500

    11 and half of 12

--- 2015/Day 11/main.py
+++ 2015/Day 11/main.py
@@ -8,15 +8,134 @@ f = open(abs_file_path, "r")
 
 data = f.read()
 
+charMap = dict()
+
+
+def setupCharMap():
+    for x in range(97, 123):
+        charMap[chr(x)] = x
+
+
+def reverseStringToList(v: str):
+    chars = [None] * len(v)
+    for x in range(len(v)):
+        chars[len(v) - x - 1] = v[x]
+    return chars
+
+
+def incrementString(v: str):
+    # print(v)
+
+    chars = reverseStringToList(v)
+
+    done = False
+    currentIndex = 0
+    while not done:
+        if chars[currentIndex] == "z":
+            chars[currentIndex] = "a"
+            currentIndex += 1
+        else:
+            chars[currentIndex] = chr(charMap[chars[currentIndex]] + 1)
+            done = True
+
+    chars = reverseStringToList(chars)
+
+    # print(chars)
+    return "".join(chars)
+
+
+def validateString(v: str):
+    if isBadLetter(v[0]) or isBadLetter(v[1]):
+        return False
+
+    has3Adj = False
+    for x in range(len(v) - 2):
+        v1 = v[x]
+        v2 = v[x + 1]
+        v3 = v[x + 2]
+
+        if isBadLetter(v3):
+            return False
+
+        n1 = charMap[v1]
+        n2 = charMap[v2]
+        n3 = charMap[v3]
+
+        if n1 + 1 == n2 and n2 + 1 == n3:
+            has3Adj = True
+            break
+
+    if not has3Adj:
+        return False
+
+    pairs = set()
+    wasLastPair = False
+    for x in range(len(v) - 1):
+        v1 = v[x]
+        v2 = v[x + 1]
+
+        if v1 == v2 and not wasLastPair:
+            pairs.add("".join([v1, v2]))
+            wasLastPair = True
+        else:
+            wasLastPair = False
+
+        if len(pairs) >= 2:
+            break
+
+    if len(pairs) < 2:
+        return False
+
+    return True
+
+
+def isBadLetter(c: str):
+    return c in ["i", "o", "l"]
+
 
 def pt1():
-    pass
+    password = data
+
+    print(incrementString(password))
+
+    done = False
+    attempt = 1
+    while not done:
+        password = incrementString(password)
+        t2 = validateString(password)
+
+        if not t2 and attempt % 10 == 0:
+            print("Attempt # {} | {}".format(attempt, password))
+
+        if t2:
+            print("Password Found in {} attempts: {}".format(attempt, password))
+            done = True
+
+        attempt += 1
 
 
 def pt2():
-    pass
+    password = "vzbxxyzz"
+
+    print(incrementString(password))
+
+    done = False
+    attempt = 1
+    while not done:
+        password = incrementString(password)
+        t2 = validateString(password)
+
+        if not t2 and attempt % 10 == 0:
+            print("Attempt # {} | {}".format(attempt, password))
+
+        if t2:
+            print("Password Found in {} attempts: {}".format(attempt, password))
+            done = True
+
+        attempt += 1
 
 
+setupCharMap()
 print("Part 1 Answer:")
 pt1()
 print("Part 2 Answer:")
--- 2015/Day 12/main.py
+++ 2015/Day 12/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 rel_path = "input.txt"
@@ -9,12 +10,61 @@ f = open(abs_file_path, "r")
 data = f.read()
 
 
+def getSum(ds):
+    if "int" in str(type(ds)):
+        return ds
+    elif "NoneType" in str(type(ds)) or "str" in str(type(ds)):
+        return 0
+    elif "list" in str(type(ds)):
+        sum = 0
+        for x in ds:
+            sum += getSum(x)
+        return sum
+    elif "dict" in str(type(ds)):
+        sum = 0
+        for x in ds.keys():
+            sum += getSum(ds[x])
+        return sum
+    else:
+        print(type(ds))
+
+
+def getSum2(ds):
+    if "int" in str(type(ds)):
+        return ds
+    elif "NoneType" in str(type(ds)) or "str" in str(type(ds)):
+        return 0
+    elif "list" in str(type(ds)):
+        sum = 0
+        for x in ds:
+            sum += getSum(x)
+        return sum
+    elif "dict" in str(type(ds)):
+        sum = 0
+
+        print(ds.values())
+
+        for x in ds.keys():
+            sum += getSum(ds[x])
+        return sum
+    else:
+        print(type(ds))
+
+
 def pt1():
-    pass
+    info = json.loads(data)
+
+    a = getSum(info)
+
+    print(a)
 
 
 def pt2():
-    pass
+    info = json.loads(data)
+
+    a = getSum2(info)
+
+    print(a)
 
 
 print("Part 1 Answer:")
