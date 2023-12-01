ca64eedf9acd4b3cbfaccff93fedd1b4dc304231
hansie4 <hans.vongruenigen@gmail.com>
2023-11-30 21:09:11 -0500

    hello

--- 2015/Day 14/main.py
+++ 2015/Day 14/main.py
@@ -1,16 +1,62 @@
 import os
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-rel_path = "input.txt"
+rel_path = "input_test.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 f = open(abs_file_path, "r")
 
-data = f.read()
+data = f.read().splitlines()
+
+
+def getStats(info):
+    deers = dict()
+
+    for x in info:
+        t = x.split()
+        name = t[0]
+        speed = int(t[3])
+        stamina = int(t[6])
+        restPeriod = int(t[13])
+
+        deers[name] = {
+            "speed": speed,
+            "stamina": stamina,
+            "restPeriod": restPeriod,
+            "cDistance": 0,
+            "cResting": False,
+            "cSpent": 0,
+        }
+
+    return deers
 
 
 def pt1():
-    pass
+    deers = getStats(data)
+
+    print(deers)
+
+    secondsToCount = 1000
+
+    for currentTime in range(secondsToCount):
+        print("Iteration: ", currentTime + 1)
+        for d in deers.keys():
+            deer = deers[d]
+
+            if deer["cResting"]:
+                if deer["cSpent"] == deer["restPeriod"]:
+                    deer["cResting"] = False
+                    deer["cSpent"] = 0
+            else:
+                if deer["cSpent"] == deer["stamina"]:
+                    deer["cResting"] = True
+                    deer["cSpent"] = 0
+
+                deer["cDistance"] = deer["cDistance"] + deer["speed"]
+
+            deer["cSpent"] = deer["cSpent"] + 1
+
+    print(deers)
 
 
 def pt2():
