4ac971b72f7ac223e67adbf5343fdf1206594437
hansie4 <hans.vongruenigen@gmail.com>
2023-12-01 13:11:29 -0500

    2023 Day 1 cleaned up code

--- 2023/Day 01/main.py
+++ 2023/Day 01/main.py
@@ -10,117 +10,10 @@ f = open(abs_file_path, "r")
 data = f.read().splitlines()
 
 
-def replaceValues(k: str, reversed: bool):
-    strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-
-    x = k
-
-    if reversed:
-        x = k[::-1]
-        for z in range(len(strings)):
-            strings[z] = strings[z][::-1]
-
-    # print(strings)
-
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    indexes = [1000000] * len(strings)
-
-    for s in range(len(strings)):
-        try:
-            indexes[s] = x.index(strings[s])
-        except:
-            indexes[s] = 1000000
-
-    # print(indexes)
-
-    firstIndex = indexes.index(min(indexes))
-
-    newString = x.replace(strings[firstIndex], str(nums[firstIndex]), 1)
-
-    if reversed:
-        return newString[::-1]
-    else:
-        return newString
-
-
-def pt1():
-    values = list()
-    for x in data:
-        # print("Checking ", x)
-
-        v1 = None
-        v2 = None
-        p1 = 0
-        p2 = len(x) - 1
-
-        for y in range(len(x)):
-            if x[p1].isdigit():
-                v1 = int(x[p1])
-
-            if x[p2].isdigit():
-                v2 = int(x[p2])
-
-            if v1 == None:
-                p1 += 1
-            if v2 == None:
-                p2 -= 1
-
-            if v1 and v2:
-                break
-
-        values.append((v1 * 10) + v2)
-
-    print(sum(values))
-
-
-valueDict = {
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
-
-
-def getCalibrationValue(s: str):
+def getCalibrationValue(s: str, valueDictionary: dict):
     locations = dict()
 
-    thingsToLookFor = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    ]
-
-    for x in thingsToLookFor:
+    for x in valueDictionary.keys():
         locations[x] = (None, None)
 
     # print(locations)
@@ -130,7 +23,7 @@ def getCalibrationValue(s: str):
         l.sort()
         # print(x + " | " + str(l))
         if l:
-            locations[x] = (l[0], l[-1], valueDict[x])
+            locations[x] = (l[0], l[-1], valueDictionary[x])
 
     # print(locations)
 
@@ -154,11 +47,53 @@ def getCalibrationValue(s: str):
     return (minValue * 10) + maxValue
 
 
+def pt1():
+    total = 0
+
+    valueDict = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
+
+    for x in data:
+        total += getCalibrationValue(x, valueDict)
+
+    print(total)
+
+
 def pt2():
     total = 0
 
+    valueDict = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
     for x in data:
-        total += getCalibrationValue(x)
+        total += getCalibrationValue(x, valueDict)
 
     print(total)
 
