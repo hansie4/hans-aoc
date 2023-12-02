9d43f32aebb3879704cdc68b92ec1a1f0ce9d2aa
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2023-12-02 17:46:35 -0500

    2015 Day 15 Part 2

--- 2015/Day 15/main.py
+++ 2015/Day 15/main.py
@@ -2,7 +2,7 @@ import os
 
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-rel_path = "input_test.txt"
+rel_path = "input.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 f = open(abs_file_path, "r")
@@ -85,11 +85,19 @@ def getScore(ingredientStats: dict, ingredientAmounts: dict):
 def getCalorieTotal(ingredientStats: dict, ingredientAmounts: dict):
     ingredients = list(ingredientStats.keys())
     calorieAmounts = list(ingredientStats.keys())
+    final = list(ingredientStats.keys())
 
-    for x in range(ingredients):
+    for x in range(len(ingredients)):
         calorieAmounts[x] = ingredientStats[ingredients[x]]["calories"]
 
-    print(calorieAmounts)
+    for x in range(len(ingredients)):
+        final[x] = calorieAmounts[x] * ingredientAmounts[ingredients[x]]
+
+    # print(calorieAmounts)
+    # print(ingredientAmounts)
+    # print(final)
+
+    return sum(final)
 
 
 def testAmount(ingredientList: list, ingredientStats: dict, values: list):
@@ -100,6 +108,25 @@ def testAmount(ingredientList: list, ingredientStats: dict, values: list):
 
     for x in range(len(ingredientList)):
         testAmounts[ingredientList[x]] = values[x]
+
+    score = getScore(ingredientStats, testAmounts)
+
+    # print(f"{score} | {testAmounts}")
+
+    return score
+
+
+def testAmountPt2(ingredientList: list, ingredientStats: dict, values: list):
+    testAmounts = dict()
+
+    if sum(values) != MAX_TEASPOONS:
+        print("INVALID VALUES")
+
+    for x in range(len(ingredientList)):
+        testAmounts[ingredientList[x]] = values[x]
+
+    if getCalorieTotal(ingredientStats, testAmounts) != 500:
+        return 0
 
     score = getScore(ingredientStats, testAmounts)
 
@@ -143,25 +170,21 @@ def pt1():
 def pt2():
     ingredientStats = getCookieStats(data, True)
 
-    print(ingredientStats)
-
     ingredientList = list(ingredientStats.keys())
 
-    print(getCalorieTotal(ingredientStats, {"Butterscotch": 40, "Cinnamon": 60}))
+    testAmounts = generateTestValues(MAX_TEASPOONS)
 
-    # testAmounts = generateTestValues(MAX_TEASPOONS)
+    topScore = 0
 
-    # topScore = 0
+    for x in testAmounts:
+        s = testAmountPt2(ingredientList, ingredientStats, x)
+        if s > topScore:
+            topScore = s
 
-    # for x in testAmounts:
-    #     s = testAmount(ingredientList, ingredientStats, x)
-    #     if s > topScore:
-    #         topScore = s
-
-    # print(topScore)
+    print(topScore)
 
 
 print("Part 1 Answer:")
-# pt1()
+pt1()
 print("Part 2 Answer:")
 pt2()
