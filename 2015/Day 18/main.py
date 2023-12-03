bf07b97c16b2510784d0565187b0215755b9ab05
hansie4 <hans.vongruenigen@gmail.com>
2023-12-03 10:29:35 -0500

    2015 Day 18 Pt 1 start

--- 2015/Day 18/main.py
+++ 2015/Day 18/main.py
@@ -1,12 +1,16 @@
 import os
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-rel_path = "input.txt"
+rel_path = "input_test.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 f = open(abs_file_path, "r")
 
-data = f.read()
+data = f.read().splitlines()
+
+
+def getNextLightState(board: list, x: int, y: int):
+    currentState = board[y][x]
 
 
 def pt1():
