68012928e7ca89ea3ab8ec944e5ca35f38c5f18f
hansie4 <hans.vongruenigen@gmail.com>
2023-12-02 20:28:35 -0500

    Added Day 3 boiler plate

---
+++ 2023/Day 03/main.py
@@ -0,0 +1,23 @@
+import os
+
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+rel_path = "input.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+f = open(abs_file_path, "r")
+
+data = f.read().splitlines()
+
+
+def pt1():
+    pass
+
+
+def pt2():
+    pass
+
+
+print("Part 1 Answer:")
+pt1()
+print("Part 2 Answer:")
+pt2()
