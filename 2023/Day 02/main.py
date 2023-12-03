2ff23a25999cc8f909249a465889e98d56e96044
hansie4 <hans.vongruenigen@gmail.com>
2023-12-02 20:27:42 -0500

    2023 Day 2

--- 2023/Day 02/main.py
+++ 2023/Day 02/main.py
@@ -9,12 +9,68 @@ f = open(abs_file_path, "r")
 data = f.read().splitlines()
 
 
+def getValidGames(maxes: dict, info: list):
+    validGames = [True] * len(info)
+
+    for y in range(len(info)):
+        x = info[y]
+        t = x.split(":")[1].replace(",", "").replace(";", "").split()
+        # print(t)
+
+        for z in range(0, len(t), 2):
+            num = int(t[z])
+            color = t[z + 1]
+
+            if maxes[color] < num:
+                validGames[y] = False
+
+    return validGames
+
+
+def getPower(game: str):
+    maxes = {"red": -1, "blue": -1, "green": -1}
+
+    t = game.split(":")[1].split(";")
+
+    # print(game)
+
+    for g in t:
+        t2 = g.replace(",", "").split()
+        # print(t2)
+
+        for z in range(0, len(t2), 2):
+            num = int(t2[z])
+            color = t2[z + 1]
+
+            if maxes[color] < num:
+                maxes[color] = num
+
+    return maxes["red"] * maxes["green"] * maxes["blue"]
+
+
 def pt1():
-    pass
+    maxes = {"red": 12, "blue": 14, "green": 13}
+
+    validGames = getValidGames(maxes, data)
+
+    # print(validGames)
+
+    ans = 0
+
+    for x in range(len(validGames)):
+        if validGames[x]:
+            ans += x + 1
+
+    print(ans)
 
 
 def pt2():
-    pass
+    total = 0
+
+    for x in data:
+        total += getPower(x)
+
+    print(total)
 
 
 print("Part 1 Answer:")
