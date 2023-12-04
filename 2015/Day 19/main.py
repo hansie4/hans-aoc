a2f23539d0f32efadbb65b822807e83587c55e98
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2023-12-04 07:27:43 -0500

    2015 Day 19

--- 2015/Day 19/main.py
+++ 2015/Day 19/main.py
@@ -3,7 +3,7 @@ import re
 import math
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-rel_path = "input.txt"
+rel_path = "input_test.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 f = open(abs_file_path, "r")
