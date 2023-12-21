30da582d8c40f15acb68a86e1b2452901e57b4c9
hansie4 <hans.vongruenigen@gmail.com>
2023-12-21 11:45:34 -0500

    Cleaning up day 16

--- 2023/Day 16/main.py
+++ 2023/Day 16/main.py
@@ -1,6 +1,9 @@
 import os
 import time
 from colorama import Fore, Style
+from collections import deque
+
+from numpy import isin
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 rel_path = "input.txt"
@@ -9,6 +12,46 @@ abs_file_path = os.path.join(script_dir, rel_path)
 f = open(abs_file_path, "r")
 
 data = f.read().splitlines()
+
+DIRECTION_MAP = {
+    (-1, 0): {
+        "/": [(0, 1)],
+        "\\": [(0, -1)],
+        "-": [(-1, 0)],
+        "|": [(0, -1), (0, 1)],
+        ".": [(-1, 0)],
+    },
+    (1, 0): {
+        "/": [(0, -1)],
+        "\\": [(0, 1)],
+        "-": [(1, 0)],
+        "|": [(0, -1), (0, 1)],
+        ".": [(1, 0)],
+    },
+    (0, -1): {
+        "/": [(1, 0)],
+        "\\": [(-1, 0)],
+        "-": [(1, 0), (-1, 0)],
+        "|": [(0, -1)],
+        ".": [(0, -1)],
+    },
+    (0, 1): {
+        "/": [(-1, 0)],
+        "\\": [(1, 0)],
+        "-": [(1, 0), (-1, 0)],
+        "|": [(0, 1)],
+        ".": [(0, 1)],
+    },
+}
+
+
+def getInitEnergizedGrid(grid: list):
+    energized = list()
+
+    for y in range(len(grid)):
+        energized.append([False] * len(grid[y]))
+
+    return energized
 
 
 def isInBounds(node: tuple, maxX: int, maxY: int):
@@ -21,147 +64,63 @@ def isInBounds(node: tuple, maxX: int, maxY: int):
     return True
 
 
-def getAllPossibleStarts(maxX: int, maxY: int):
+def getNumEnergized(grid: list, start: tuple):
+    visited = set()
+
+    queue = deque()
+    queue.append(start)
+
+    while queue:
+        x, y, dX, dY = queue.popleft()
+
+        newX = x + dX
+        newY = y + dY
+
+        if isInBounds((newX, newY), len(grid[0]) - 1, len(grid) - 1):
+            nextChar = grid[newY][newX]
+
+            nextDirs = DIRECTION_MAP[(dX, dY)][nextChar]
+
+            for dir in nextDirs:
+                if (newX, newY, dir[0], dir[1]) not in visited:
+                    visited.add((newX, newY, dir[0], dir[1]))
+                    queue.append((newX, newY, dir[0], dir[1]))
+
+    # print(visited)
+
+    return len(set(map(lambda x: (x[0], x[1]), visited)))
+
+
+def getAllPossibleStarts(grid: list):
     starts = set()
 
-    for x in range(maxX + 1):
-        s1 = (x, 0, "D")
-        s2 = (x, maxY, "U")
-        starts.add(s1)
-        starts.add(s2)
+    for x in range(len(grid[0])):
+        starts.add((x, -1, 0, 1))
+        starts.add((x, len(grid), 0, -1))
 
-    for y in range(maxY + 1):
-        s3 = (0, y, "R")
-        s4 = (maxX, y, "L")
-        starts.add(s3)
-        starts.add(s4)
+    for y in range(len(grid)):
+        starts.add((-1, y, 1, 0))
+        starts.add((len(grid[0]), y, -1, 0))
 
     return starts
 
 
-def getEnergizedSquares(start: tuple):
-    currentNodes = set()
-    currentNodes.add(start)
-
-    energized = set()
-    seen = set()
-
-    seen.add((start[0], start[1]))
-    energized.add((start[0], start[1]))
-
-    while True:
-        cn = currentNodes.copy()
-
-        if len(cn) == 0:
-            # print("DONE")
-            break
-
-        for n in cn:
-            seen.add(n)
-            energized.add((n[0], n[1]))
-            currentDir = n[2]
-            if currentDir == "U":
-                nextNode = [n[0], n[1] - 1, n[2]]
-            elif currentDir == "D":
-                nextNode = [n[0], n[1] + 1, n[2]]
-            elif currentDir == "L":
-                nextNode = [n[0] - 1, n[1], n[2]]
-            else:
-                nextNode = [n[0] + 1, n[1], n[2]]
-
-            if isInBounds(nextNode, len(data[0]) - 1, len(data) - 1):
-                nextLocValue = data[nextNode[1]][nextNode[0]]
-
-                nextDir = None
-                nextDirs = None
-
-                if nextLocValue == "\\":
-                    if currentDir == "U":
-                        nextDir = "L"
-                    elif currentDir == "D":
-                        nextDir = "R"
-                    elif currentDir == "L":
-                        nextDir = "U"
-                    elif currentDir == "R":
-                        nextDir = "D"
-                    else:
-                        print("THIS SHOULD NEVER HAPPEN 1")
-                elif nextLocValue == "/":
-                    if currentDir == "U":
-                        nextDir = "R"
-                    elif currentDir == "D":
-                        nextDir = "L"
-                    elif currentDir == "L":
-                        nextDir = "D"
-                    elif currentDir == "R":
-                        nextDir = "U"
-                    else:
-                        print("THIS SHOULD NEVER HAPPEN 2")
-                elif nextLocValue == "-" and currentDir in ["U", "D"]:
-                    nextDirs = ["L", "R"]
-                elif nextLocValue == "|" and currentDir in ["L", "R"]:
-                    nextDirs = ["U", "D"]
-                else:
-                    nextDir = currentDir
-
-                nextNodes = []
-
-                if nextDir != None:
-                    nextNode[2] = nextDir
-                elif nextDirs != None:
-                    for d in nextDirs:
-                        nextNodes.append((nextNode[0], nextNode[1], d))
-                else:
-                    print("THIS SHOULD NEVER HAPPEN 3")
-
-                if len(nextNodes) > 0:
-                    for z in nextNodes:
-                        t = tuple(z)
-                        if t not in seen:
-                            currentNodes.add(t)
-                            energized.add((t[0], t[1]))
-                else:
-                    t = tuple(nextNode)
-                    if t not in seen:
-                        currentNodes.add(t)
-                        energized.add((t[0], t[1]))
-
-            currentNodes.remove(n)
-
-        # energizedSpots = list(map(lambda x: (x[0], x[1]), list(energized)))
-        # os.system("cls" if os.name == "nt" else "clear")
-        # for y in range(len(data)):
-        #     for x in range(len(data[y])):
-        #         if (x, y) in energizedSpots:
-        #             print("#", end="")
-        #         else:
-        #             print(".", end="")
-        #     print()
-
-        # input()
-
-    ans = len(energized)
-    return ans
-
-
 def pt1():
-    start = (0, 0, "R")
-
-    ans = getEnergizedSquares(start)
-
-    return ans
+    start = (-1, 0, 1, 0)
+    energized = getNumEnergized(data, start)
+    return energized
 
 
 def pt2():
-    starts = getAllPossibleStarts(len(data[0]) - 1, len(data) - 1)
+    ans = -1
 
-    ans = 0
+    allPossibleStarts = getAllPossibleStarts(data)
 
-    for s in starts:
-        t = getEnergizedSquares(s)
+    for start in allPossibleStarts:
+        energized = getNumEnergized(data, start)
 
-        if t > ans:
-            ans = t
+        if energized > ans:
+            ans = energized
 
     return ans
 
